ac313ac764680e758dfddc79bd7a2c2880e4cb4f
kkew3 <kps6326@hotmail.com>
2024-03-08 11:23:43 +0800

    Restructure the project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='stackexchange-search',
+    package_dir={'': 'src'},
+    packages=[
+        'stackexchange_search',
+    ],
+    version='2.1.1',
+)
